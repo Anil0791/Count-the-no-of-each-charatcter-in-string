cac51de55522a559e215e1c1fa2909a8a699147a
Anil Patidar <patidaranil0791@gmail.com>
2018-12-18 00:56:10 +0530

    Update Solutions.py

--- Solutions.py
+++ Solutions.py
@@ -1,11 +1,11 @@
 def countex(str):
-    str.casefold()      #remove effect of uppercase n lowercase
+    str.casefold()                                      #remove effect of uppercase n lowercase
     l = []
-    n = len (str)   #count lenght of string
+    n = len (str)                                       #count lenght of string
     for i in range (n):
-        l.append (str[i])      #take string character into list
-    d = {x: l.count (x) for x in l}       #remove the duplicate count n store key n value
-    d={k:round((v*100/n),2) for(k,v) in d.items()}   #perform opearation on value to convert into percentage
+        l.append (str[i])                               #take string character into list
+    d = {x: l.count (x) for x in l}                     #remove the duplicate count n store key n value
+    d={k:round((v*100/n),2) for(k,v) in d.items()}      #perform opearation on value to convert into percentage
     print(d)
     
 
